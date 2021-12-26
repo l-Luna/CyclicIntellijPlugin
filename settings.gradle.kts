054c80b598072a82e9c85d35326f48bc82dfc7aa
Luna <izzeldeen03@gmail.com>
2021-12-26 21:46:40 +0000

    setup JPS plugin, but don't use it: cyclic compiler requires (unavailable) JDK 16

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "CyclicIntellijPlugin"
-
+//include("jps-plugin")
--- src/main/java/cyclic/intellij/CyclicIcons.java
+++ src/main/java/cyclic/intellij/CyclicIcons.java
@@ -7,4 +7,5 @@ import javax.swing.*;
 public class CyclicIcons{
 	
 	public static final Icon CYCLIC_FILE = IconLoader.getIcon("cyclic_file.svg", CyclicIcons.class);
+	public static final Icon CYCLIC_CLASS = IconLoader.getIcon("cyclic_class.svg", CyclicIcons.class);
 }
