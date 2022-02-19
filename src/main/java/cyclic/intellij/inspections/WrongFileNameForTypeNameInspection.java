c97d2189b0a4069e54480f03f3179b88eb0bf153
Luna <izzeldeen03@gmail.com>
2022-02-19 14:55:29 +0000

    fix error when type has no defined name

--- src/main/java/cyclic/intellij/inspections/WrongFileNameForTypeNameInspection.java
+++ src/main/java/cyclic/intellij/inspections/WrongFileNameForTypeNameInspection.java
@@ -14,10 +14,11 @@ public class WrongFileNameForTypeNameInspection extends CyclicInspection{
 	
 	public ProblemDescriptor @NotNull [] checkTypeDef(@NotNull CycType type, @NotNull InspectionManager manager, boolean isOnTheFly){
 		String typeName = type.getName();
-		if(type.isTopLevelType() && type.getNameIdentifier() != null){
-			if(!typeName.equals(type.getContainingFile().getName()))
-				return new ProblemDescriptor[]{ manager.createProblemDescriptor(type.getNameIdentifier(), "Cyclic type '" + typeName + "' should be declared in file '" + typeName + ".cyc'", new LocalQuickFix[]{ new RenameFileToTypeFix(type, typeName), new RenameTypeToFileFix(type, type.getContainingFile().getName()) }, ProblemHighlightType.ERROR, isOnTheFly, false) };
-		}
+		if(type.isTopLevelType() && type.getNameIdentifier() != null)
+			if(type.getNameIdentifier().getTextLength() > 0){
+				if(!typeName.equals(type.getContainingFile().getName()))
+					return new ProblemDescriptor[]{manager.createProblemDescriptor(type.getNameIdentifier(), "Cyclic type '" + typeName + "' should be declared in file '" + typeName + ".cyc'", new LocalQuickFix[]{new RenameFileToTypeFix(type, typeName), new RenameTypeToFileFix(type, type.getContainingFile().getName())}, ProblemHighlightType.ERROR, isOnTheFly, false)};
+			}else return new ProblemDescriptor[]{manager.createProblemDescriptor(type, "Cyclic type must have name '" + type.getContainingFile().getName() + "'", new LocalQuickFix[]{}, ProblemHighlightType.GENERIC_ERROR, isOnTheFly, false)};
 		return super.checkTypeDef(type, manager, isOnTheFly);
 	}
 	
