fdbc4bb037c174d39b243a240ff0466c45b9eeb7
Luna <izzeldeen03@gmail.com>
2022-03-13 12:38:47 +0000

    improve folding code

--- src/main/java/cyclic/intellij/presentation/CyclicFoldingBuilder.java
+++ src/main/java/cyclic/intellij/presentation/CyclicFoldingBuilder.java
@@ -13,8 +13,9 @@ import cyclic.intellij.psi.Tokens;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.List;
 
 public class CyclicFoldingBuilder extends FoldingBuilderEx implements DumbAware{
 	
@@ -24,14 +25,12 @@ public class CyclicFoldingBuilder extends FoldingBuilderEx implements DumbAware{
 	
 	public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull ASTNode node, @NotNull Document document, boolean quick){
 		// we don't use `quick` yet, but using FoldingBuilderEx means we can fold immediately
-		FoldingDescriptor[] descriptors = new FoldingDescriptor[0];
+		List<FoldingDescriptor> collected = new ArrayList<>();
 		if(node.getElementType() == Tokens.getRuleFor(CyclicLangParser.RULE_classDecl) || node.getElementType() == Tokens.getRuleFor(CyclicLangParser.RULE_block)){
 			var opBrace = node.findChildByType(Tokens.getFor(CyclicLangLexer.LBRACE));
 			var endBrace = node.findChildByType(Tokens.getFor(CyclicLangLexer.RBRACE));
-			if(opBrace != null && endBrace != null){
-				descriptors = new FoldingDescriptor[]{
-						new FoldingDescriptor(node, TextRange.create(opBrace.getStartOffset(), endBrace.getStartOffset() + 1))};
-			}
+			if(opBrace != null && endBrace != null)
+				collected.add(new FoldingDescriptor(node, TextRange.create(opBrace.getStartOffset(), endBrace.getStartOffset() + 1)));
 		}
 		if(node.getElementType() == Tokens.getRuleFor(CyclicLangParser.RULE_imports)){
 			var imports = node.getChildren(Tokens.RULE_IMPORT);
@@ -39,21 +38,15 @@ public class CyclicFoldingBuilder extends FoldingBuilderEx implements DumbAware{
 				// fold from first ID to last semicolon
 				var begin = imports[0].findChildByType(Tokens.getRuleFor(CyclicLangParser.RULE_id));
 				var end = imports[imports.length - 1].findChildByType(Tokens.getFor(CyclicLangLexer.SEMICOLON));
-				if(begin != null && end != null){
-					descriptors = new FoldingDescriptor[]{
-							new FoldingDescriptor(node, TextRange.create(begin.getStartOffset(), end.getStartOffset() + 1))};
-				}
+				if(begin != null && end != null)
+					collected.add(new FoldingDescriptor(node, TextRange.create(begin.getStartOffset(), end.getStartOffset() + 1)));
 			}
 		}
-		// simple recursive approach
-		// just add the arrays
-		descriptors = Stream.concat(
-				Arrays.stream(node.getChildren(null))
-					.map(x -> buildFoldRegions(x, document))
-					.flatMap(Arrays::stream),
-				Arrays.stream(descriptors))
-					.toArray(FoldingDescriptor[]::new);
-		return descriptors;
+		Arrays.stream(node.getChildren(null))
+				.map(x -> buildFoldRegions(x, document))
+				.flatMap(Arrays::stream)
+				.forEach(collected::add);
+		return collected.toArray(FoldingDescriptor[]::new);
 	}
 	
 	public @Nullable String getPlaceholderText(@NotNull ASTNode node){
