9b419b0ef7579a733a240b3028e446f3339d38ae
Luna <izzeldeen03@gmail.com>
2022-04-10 18:12:12 +0100

    display cyclic icon on cyclic project files

---
+++ src/main/java/cyclic/intellij/projects/CyclicProjectYamlFilePatcher.java
@@ -0,0 +1,21 @@
+package cyclic.intellij.projects;
+
+import com.intellij.ide.FileIconPatcher;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import cyclic.intellij.CyclicIcons;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class CyclicProjectYamlFilePatcher implements FileIconPatcher, DumbAware{
+	
+	public static final String PROJECT_YAML_EXTENSION = ".cyc.yaml";
+	
+	public Icon patchIcon(Icon baseIcon, VirtualFile file, int flags, @Nullable Project project){
+		if(file.getName().endsWith(PROJECT_YAML_EXTENSION))
+			return CyclicIcons.CYCLIC_FILE; // TODO: better icon
+		return baseIcon;
+	}
+}
