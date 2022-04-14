3af347ce3412860c80a0a4a12719a60d6dc70b50
Luna <izzeldeen03@gmail.com>
2022-04-14 15:40:39 +0100

    update for IJ 2022.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,44 @@
-plugins {
-    id("org.jetbrains.intellij") version "1.3.0"
-    //id("antlr") // TODO: Use the plugin to build the parser automatically - requires splitting parser and lexer
-    java
+plugins{
+	id("org.jetbrains.intellij") version "1.5.2"
+	// TODO: Use the plugin to build the parser automatically - requires splitting parser and lexer
+	//id("antlr")
+	java
 }
 
 group = "com.github.l-Luna.CyclicIntellijPlugin"
-version = "0.0.2"
+version = "0.0.3"
 
-repositories {
-    mavenCentral()
+repositories{
+	mavenCentral()
 }
 
-dependencies {
-    implementation("org.antlr:antlr4-runtime:4.9.3")
-    implementation("org.antlr:antlr4-intellij-adaptor:0.1")
-    //implementation("org.antlr:antlr4-master:4.9.3")
-    
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+dependencies{
+	implementation("org.antlr:antlr4-runtime:4.9.3")
+	implementation("org.antlr:antlr4-intellij-adaptor:0.1")
+	//implementation("org.antlr:antlr4-master:4.9.3")
+	
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    version.set("2021.3")
-    plugins.add("com.intellij.java")
-    plugins.add("yaml")
-    plugins.add("coverage")
+intellij{
+	version.set("2022.1")
+	plugins.add("com.intellij.java")
+	plugins.add("yaml")
+	plugins.add("coverage")
 }
-tasks {
-    patchPluginXml {
-        changeNotes.set("""
-            First version.       """.trimIndent())
-    }
+
+tasks{
+	patchPluginXml{
+		changeNotes.set(
+			"""
+            First version.
+			""".trimIndent()
+		)
+	}
 }
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+
+tasks.getByName<Test>("test"){
+	useJUnitPlatform()
 }
